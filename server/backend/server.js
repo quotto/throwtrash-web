36f5bc3486b243702db4e9d28587f37e938deb63
My Name <my-name@chromium.org>
2018-03-16 23:59:12 +0900

    fix server route(/oauth/request_token)

--- server/backend/server.js
+++ server/backend/server.js
@@ -75,6 +75,8 @@ app.get('/oauth/request_token',(req,res)=>{
     req.session.client_id = req.query.client_id
     req.session.vendor_id = req.query.vendor_id
     req.session.redirect_uri = req.query.redirect_uri
+
+    res.redirect('/index.html')
 });
 
 app.post("/trashes",(req,res)=> {
