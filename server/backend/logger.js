83e006db1da314b51d5f36e9599a0151b8534625
My Name <my-name@chromium.org>
2018-03-19 23:27:02 +0900

    fix create server logger,skill set sessionendrequest

---
+++ server/backend/logger.js
@@ -0,0 +1,20 @@
+class Logger {
+    constructor(filename){
+        this.filename=filename
+    }
+    write(msg,notice) {
+            const dt = new Date()
+            if(notice) {
+                console.log(`${this.writetime()} [${notice}] ${msg}`)
+            } else {
+                console.log(`${this.writetime()} ${msg}`)
+            }
+        }
+
+    writetime(){
+        const dt = new Date()
+        return `${dt.getFullYear()}/${dt.getMonth()+1}/${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()} -`
+    }
+}
+
+module.exports = Logger
--- server/backend/server.js
+++ server/backend/server.js
@@ -9,6 +9,9 @@ const session = require('express-session')
 const cookieParser = require('cookie-parser')
 const express = require('express')
 const Util = require('./utility.js')
+const Logger = require('./logger.js')
+
+var logger = new Logger('./server.log')
 
 //. 鍵ファイルと証明書ファイル
 var options = {
@@ -24,13 +27,6 @@ app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(session({secret: process.env.TRASHES_SECRET}))
 
-var credential = new aws.Credentials(process.env.AWS_ACCESS_TOKEN,process.env.AWS_ACCESS_TOKEN_SECRET,null)
-const dynamoClient = new aws.DynamoDB.DocumentClient({
-    region:'ap-northeast-1',
-    apiVersion: '2012-08-10',
-    credentials: credential
-})
-
 var mime = {
     '.html':'text/html',
     '.css':'text/css',
@@ -40,19 +36,18 @@ var mime = {
 //. 鍵ファイルと証明書ファイルを指定して、https で待受け
 const port = process.argv[2] ? process.argv[2] : 443
 const server = https.createServer( options, app ).listen(port, ()=>{
-    console.log( "server stating on " + server.address().port + " ..." );
+    logger.write( "server is starting on " + server.address().port + " ..." )
 });
 
 app.get(/.+\..+/,(req,res,next)=>{
     let requestPath = url.parse(req.url).pathname;
-    console.log(`->${req.method} ${requestPath}`);
+    logger.write(`->${req.method} ${requestPath}`,"REQ")
 
     let sendResource = path.join('./public',requestPath)
     let sendFile = fs.createReadStream(sendResource,{encoding:'utf-8'})
     let responseData = ''
     sendFile.on("readable",()=>{
         res.set({"Content-Type":mime[path.extname(sendResource)]})
-        console.log(mime[path.extname(sendResource)])
     })
 
     sendFile.on("data",(data)=>{
@@ -62,11 +57,11 @@ app.get(/.+\..+/,(req,res,next)=>{
     sendFile.on("close",()=> {
         res.send(responseData)
         res.status(200).end()
-        console.log(`<- OK:${req.method} ${sendResource}`)
+        logger.write(`<- OK:${req.method} ${sendResource}`,"RES")
     })
 
     sendFile.on("error",(err)=>{
-        console.log(`<- Error:${req.method} ${requestPath}`)
+        logger.write(`<- Error:${req.method} ${requestPath}`,"ERROR")
     })
 })
 
@@ -75,47 +70,40 @@ app.get('/oauth/request_token',(req,res)=>{
     req.session.client_id = req.query.client_id
     req.session.vendor_id = req.query.vendor_id
     req.session.redirect_uri = req.query.redirect_uri
+    if(!req.session.state || !req.session.client_id || !req.session.vendor_id || req.session.redirect_uri) {
+        logger.write("Bad Request","ERROR")
+        res.status(400).end("bad request")
+        return
+    }
 
     res.redirect('/index.html')
 });
 
-app.post("/trashes",(req,res)=> {
-    var user_id = req.body['user_id']
-    var params = {
-        TableName: 'TrashSchedule',
-        Key: {
-            id: user_id
-        }
-    }
-    dynamoClient.get(params,(err,data)=>{
-        if(err || typeof(data.Item)==="undefined") {
-            console.log("DB get error")
-            console.log(err)
-            res.status(500).end("情報の取得に失敗しました。スキル開発者にお問い合わせください。")
-        } else {
-            console.log(data)
-            res.status(200).end(JSON.stringify(data))
-        }
-    })
-})
-
 app.post("/regist",(req,res,next)=>{
-    const user_id = Util.create_id()
-    var item = {
-        id: user_id,
-        description: JSON.stringify(req.body,null,2)
-    }
-    var params = {
-        TableName: 'TrashSchedule',
-        Item: item
-    }
-    dynamoClient.put(params,(err,data)=>{
-        if(err) {
-            console.log(err)
-            res.status(500).end("registraion error")
-        } else {
-            const redirect_url = `${req.session.redirect_uri}#state=${req.session.state}&access_token=${user_id}&client_id=${req.session.client_id}&token_type=Bearer`
-            res.status(200).end(redirect_url)
+    if(!req.session.state || !req.session.client_id || !req.session.vendor_id || req.session.redirect_uri) {
+        logger.write("Bad Request","ERROR")
+        res.status(400).end("bad request")
+        return
+    } else {
+
+        const user_id = Util.create_id()
+        var item = {
+            id: user_id,
+            description: JSON.stringify(req.body,null,2)
         }
-    })
+        var params = {
+            TableName: 'TrashSchedule',
+            Item: item
+        }
+        dynamoClient.put(params,(err,data)=>{
+            if(err) {
+                logger.write(`DB Insert Error\n${err}`,"ERROR")
+                res.status(500).end("registraion error")
+            } else {
+                logger.write(`Regist user(${user_id}\n${JSON.stringify(req.body,null,2)})`,"INFO")
+                const redirect_url = `${req.session.redirect_uri}#state=${req.session.state}&access_token=${user_id}&client_id=${req.session.client_id}&token_type=Bearer`
+                res.status(200).end(redirect_url)
+            }
+        })
+    }
 })
