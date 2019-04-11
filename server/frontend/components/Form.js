18752950833106450b56a0e0f99a568b7c3e5ba1
quotto <rhyme.and.rhyme@gmail.com>
2019-04-12 00:12:56 +0900

    [fix]ロケール対応 URLで指定されたlangに対応する画面構成に変更。チェック処理は未対応。

--- server/frontend/components/Form.js
+++ server/frontend/components/Form.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ScheduleListContainer from '../containers/ScheduleListContainer';
-import {Field, Form,reduxForm} from 'redux-form'
 
 class  SubmitForm extends React.Component {
     render() {
