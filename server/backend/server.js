8796b5434f2f19c086bcd3800dd218b50b5e10f3
quotto <rhyme.and.rhyme@gmail.com>
2019-09-22 00:00:27 +0900

    サーバモジュールの実行モードでbundleパスを変更するように修正

--- server/backend/server.js
+++ server/backend/server.js
@@ -12,6 +12,7 @@ const MetaInfo = require('./public/meta.json');
 const CONFIG = require('./config.json');
 
 const args = require('args');
+args.option('mode', 'production or debug', 'production');
 args.option('level', 'logger level', 'info');
 args.option('port', 'listening port', 8888);
 const argv = args.parse(process.argv);
@@ -34,9 +35,6 @@ log4js.configure({
     }
 });
 const logger = log4js.getLogger();
-const SIGNIN_KEY = 'signing';
-
-const lineOauth_endpoint = 'https://todays-trash.herokuapp.com/oauth/request_token';
 
 const errorRedirect = (res, code, message)=>{
     res.set('Content-Type', 'text/plain;charset=utf-8');
@@ -49,20 +47,15 @@ const createNewId = async()=>{
     let retry = 0;
     while(!user_id || retry < 5) {
         user_id = Util.create_id();
-        const option = {
-            uri: `${CONFIG.api.endpoint}/data`,
-            method: 'GET',
-            qs: {
+        logger.debug('create new id request:');
+        const result = await dynamoClient.get({
+            TableName: 'TrashSchedule',
+            Key: {
                 id: user_id
-            },
-            headers: {
-                'x-api-key': CONFIG.api.token
             }
-        };
-        logger.debug('create new id request:', option);
-        const response = await rp(option);
-        logger.debug(response);
-        if(response === '') {
+        }).promise();
+        logger.debug(result);
+        if(!result.Item) {
             logger.debug('create new id:', user_id);
             break;  
         }
@@ -71,33 +64,75 @@ const createNewId = async()=>{
     return user_id;
 };
 
-const getIdFromLineId = async(platform, lineId)=>{
-    let user_id = null;
-    const option = {
-        uri: `${CONFIG.api.endpoint}/data`,
-        method: 'GET',
-        qs: {
-            lineId: lineId
-        },
-        headers: {
-            'x-api-key': CONFIG.api.token
-        }
+const getIdBySignId = (signinId)=>{
+    const params = {
+        TableName: 'TrashSchedule',
+        IndexName: 'signinId-index',
+        ExpressionAttributeNames: { '#i': 'signinId' } ,
+        ExpressionAttributeValues: { ':val': signinId },
+        KeyConditionExpression: '#i = :val'
     };
-    logger.debug('get id from lineId request', option);
-    const response = await rp(option);
-    const body = JSON.parse(response);
-    // 登録済みデータがある場合はidを再利用してUPDATE
-    if (body.length > 0) {
-        user_id = body[0].id;
-        logger.debug('get id from lineId:', user_id);
-    }
-    return user_id;
+    return dynamoClient.query(params).promise().then(data => {
+        logger.debug(data);
+        if(data.Items.length > 0) {
+            return {id: data.Items[0].id, preset: JSON.parse(data.Items[0].description)};
+        }
+        return {};
+    }).catch(err=>{
+        logger.error(err);
+        return {};
+    });
+};
+
+const requestAmazonProfile = async(access_token)=>{
+    return rp({
+        uri: 'https://api.amazon.com/user/profile',
+        qs: {
+            access_token: access_token
+        },
+        resolveWithFullResponse: true,
+        json: true
+    }).then(response => {
+        logger.debug(JSON.stringify(response));
+        if (response.statusCode === 200) {
+            return {id: response.body.user_id, name: response.body.name};
+        }
+        Promise.reject(response);
+    }).catch(err=>{
+        logger.error(err);
+    });
+};
+
+const requestGoogleProfile = async(code)=>{
+    const options = {
+        uri: 'https://oauth2.googleapis.com/token',
+        method: 'POST',
+        body: {
+            code: code,
+            client_id: CONFIG.google.client_id,
+            client_secret: CONFIG.google.client_secret,
+            redirect_uri: `${CONFIG.base.endpoint}/signin?service=google`,
+            grant_type: 'authorization_code'
+        },
+        json: true
+    };
+    return rp(options).then(response=>{
+        if(response.id_token) {
+            const decoded_token = jwt.decode(response.id_token);
+            logger.debug(decoded_token);
+            return {id: decoded_token.sub, name: decoded_token.name};
+        } else {
+            Promise.reject(response);
+        }
+    }).catch(err=>{
+        logger.error(err);
+    });
 };
 
 const generateState = (length)=>{
     let state = '';
     for(let i=0; i<length; i++) {
-        state += 'abcdefghijklmnopqrstuvwxyz0123456789'[Math.floor(Math.random()*35)]
+        state += 'abcdefghijklmnopqrstuvwxyz0123456789'[Math.floor(Math.random()*35)];
     }
     return state;
 };
@@ -126,25 +161,23 @@ firebase_admin.initializeApp({
 let firestore = firebase_admin.firestore();
 
 const server = http.createServer(app).listen(argv.port, ()=>{
-    logger.info( 'server is starting on ' + server.address().port + ',run level:' + argv.level + ' ...' );
+    logger.info( 'server is starting on ' + server.address().port + ',run level:' + argv.level + ',mode: ' + argv.mode + ' ...' );
 });
 
 app.use('/', express.static('./public'));
 
 app.get('/index/:version/:lang',(req,res)=>{
     const lang = req.params.lang;
-    //テスト用
-    req.session.version = req.params.version;
     if(MetaInfo[lang]) {
-        // ユーザー側にsigninフラグが存在した場合を考慮してセッション上にsigninIdが無ければcookieをクリアする
-        req.session.signinId ? res.cookie(SIGNIN_KEY, true) : res.clearCookie(SIGNIN_KEY);
+        const bundle_path = argv.mode === 'production' ? 
+            `https://d29p8bq9xwgr82.cloudfront.net/bundle/v${req.params.version}.js` : `/v${req.params.version}.js`;
         res.charset = 'utf-8';
         res.header('Content-Type', 'text/html;charset=utf-8');
         res.status(200);
         res.render('index',{
             lang: lang,
             title: MetaInfo[lang].title,
-            version: req.params.version
+            bundle_path: bundle_path
         });
     } else {
         logger.warn('Wrong lang');
@@ -178,13 +211,12 @@ app.get('/oauth_signin', (req, res)=>{
     const option = {
         client_id: CONFIG.google.client_id,
         response_type:'code',
-        scope:'openid email',
+        scope:'openid profile',
         redirect_uri:`${CONFIG.base.endpoint}/signin?service=google`,
         state: req.session.googleState,
         login_hint: 'mythrowaway.net@gmail.com',
         nonce: new Date().toISOString()
     };
-    console.log(option);
     const params_array = [];
     for(let [key, value] of Object.entries(option)) {
         params_array.push(`${key}=${value}`);
@@ -193,200 +225,147 @@ app.get('/oauth_signin', (req, res)=>{
 });
 
 app.get('/signin', (req,res)=>{
+    let get_profile_task = null;
     if (req.query.service === 'amazon') {
         const access_token = req.query.access_token;
         if (access_token) {
-            return rp({
-                uri: 'https://api.amazon.com/user/profile',
-                qs: {
-                    access_token: access_token
-                },
-                resolveWithFullResponse: true,
-                json: true
-            }).then(response => {
-                logger.debug(JSON.stringify(response));
-                if (response.statusCode === 200 && req.session.version) {
-                    const user_id = response.body.user_id;
-                    res.cookie(SIGNIN_KEY, true);
-                    req.session.signinId = user_id;
-                    res.redirect(`/index/${req.session.version}/${req.acceptsLanguages('en', 'ja')}`);
-                } else {
-                    Promise.reject(response);
-                }
-            }).catch(err => {
-                logger.error(err);
-                errorRedirect(res, 500, 'エラーが発生しました');
-            });
+            get_profile_task = requestAmazonProfile(access_token);
         }
     } else if(req.query.service === 'google') {
         const code = req.query.code;
         if((req.query.state === req.session.googleState) && code) {
-            const options = {
-                uri: 'https://oauth2.googleapis.com/token',
-                method: 'POST',
-                body: {
-                    code: code,
-                    client_id: CONFIG.google.client_id,
-                    client_secret: CONFIG.google.client_secret,
-                    redirect_uri: `${CONFIG.base.endpoint}/signin?service=google`,
-                    grant_type: 'authorization_code'
-                },
-                json: true
-            };
-            return rp(options).then(response=>{
-                if(response.id_token) {
-                    const decoded_token = jwt.decode(response.id_token);
-                    res.cookie(SIGNIN_KEY, true);
-                    req.session.signinId = decoded_token.sub;
-                    res.redirect(`/index/${req.session.version}/${req.acceptsLanguages('en', 'ja')}`);
-                } else {
-                    Promise.reject(response);
-                }
-            }).catch(err=>{
-                logger.error(err);
-                errorRedirect(res, 500, 'エラーが発生しました');
-            });
+            get_profile_task = requestGoogleProfile(code);
         }
+    } else {
+        logger.error('invalid request');
+        errorRedirect(res, 400, '不正なリクエストです');
     }
-    logger.error('invalid request');
-    errorRedirect(res, 400, '不正なリクエストです');
+
+    get_profile_task.then(user_info=>{
+        req.session.userInfo = { 
+            signinId: user_info.id,
+            name: user_info.name,
+            signinService: req.query.service
+        };
+        return getIdBySignId(user_info.id);
+    }).then(data=>{
+        if(data.id) {
+            req.session.userInfo.id = data.id;
+            req.session.userInfo.preset = data.preset;
+        } else {
+            req.session.userInfo.preset = [];
+        }
+        res.redirect(`/index/${req.session.version}/${req.acceptsLanguages(['en','ja'])}`);
+    }).catch(err=>{
+        logger.errpr(err);
+        errorRedirect(res, 500, 'エラーが発生しました');
+    });
 });
 
 app.get('/signout', (req,res)=>{
-    if(req.session.signinId) {
-        logger.info('signout:'+req.session.signinId);
-        req.session.signinId = undefined;
-        res.clearCookie(SIGNIN_KEY);
+    if(req.session.userInfo) {
+        logger.info('signout:'+req.session.userInfo.signinId);
+        req.session.userInfo = undefined;
         res.json({message:'success signout'});
         return;
     }
     logger.warn('not signed in user');
-    res.status(500);
-    res.send('error');
+    res.status(401).send('not signed in.');
 });
 
 app.get('/user_info', (req,res)=>{
-    if(req.session.signinId) {
-        res.status(200).json({name: 'test'});
+    if(req.session.userInfo) {
+        res.status(200).json({ name: req.session.userInfo.name, preset: req.session.userInfo.preset  });
         return;
     }
+    logger.warn('not signed in user');
     res.status(401).json({message: 'Unauthorized'});
 });
 
 app.post('/regist',(req,res)=>{
-    if(req.session.state && req.session.client_id && req.session.redirect_uri) {
-        logger.debug('Regist request:',req.body);
-        const recv_data = JSON.parse(req.body.data);
-        if(common_check.exist_error(recv_data)) {
-            logger.error(`Bad Data\n${recv_data}`);
-            errorRedirect(res, 400, '不正なリクエストです');
-            return;
-        }
-
-        // 検証した登録データをセッションに格納
-        const regist_data = Util.adjustData(recv_data);
-        req.session.regist_data = regist_data;
-
-        // リダイレクト元の検証用にランダム値をセッションに格納
-        let redirect_state  = '';
-        for(let i=0; i<16; i++) {
-            redirect_state += 'abcdefghijklmnopqrstuvwxyz0123456789'[Math.floor(Math.random()*35)]
-        }
-        req.session.redirect_state = redirect_state;
-
-        if(req.body.line) {
-            res.status(200).end(`${lineOauth_endpoint}?&redirect_state=${redirect_state}&redirect_url=${process.env.LINE_OAUTH_CALLBACK}`);
-        } else {
-            res.status(200).end(`/submit?redirect_state=${redirect_state}`);
-        }
-        return;
-        
-    } else {
-        logger.error('Bad Request');
+    logger.debug('Regist request:',JSON.stringify(req.body));
+    const recv_data = req.body;
+    if(common_check.exist_error(recv_data)) {
+        logger.error(`Bad Data\n${recv_data}`);
         errorRedirect(res, 400, '不正なリクエストです');
         return;
     }
+
+    // 検証した登録データをセッションに格納
+    const regist_data = Util.adjustData(recv_data);
+    req.session.regist_data = regist_data;
+
+    // リダイレクト元の検証用にランダム値をセッションに格納
+    req.session.redirect_state = generateState(10);
+
+    res.redirect(`/submit?redirect_state=${req.session.redirect_state}`);
 });
 
 app.get('/submit', async(req,res)=>{
-    const query = req.query;
-    if(req.session.state && req.session.client_id && req.session.redirect_uri && query.redirect_state === req.session.redirect_state) {
-        let user_id = null;
-        const lineId = query.lineId;
-        if(lineId) {
-            try {
-                user_id = await getIdFromLineId(req.session.platform, lineId);
-            } catch(err) {
-                logger.error(err);
-                errorRedirect(res, 400, '登録に失敗しました。');
+    if(req.session.state && req.session.client_id && req.session.redirect_uri && req.query.redirect_state === req.session.redirect_state) {
+        const item = {};
+        if(req.session.userInfo) {
+            item.signinId = req.session.userInfo.signinId;
+            item.signinService = req.session.userInfo.signinService;
+            if(req.session.userInfo.id) {
+                item.id = req.session.userInfo.id;
             }
         }
-
-        if(!user_id) {
-            user_id = await createNewId();
+        if(!item.id) {
+            const user_id = await createNewId();
             if(!user_id) {
                 logger.error('Failed to create Id');
-                errorRedirect(res, 400, '登録に失敗しました。');
+                errorRedirect(res, 500, '登録に失敗しました。');
+                return;
             }
+            item.id = user_id;
         }
 
         // データ登録
         const regist_data = req.session.regist_data;
-        const item = {
-            id: user_id,
-            description: JSON.stringify(regist_data, null, 2),
-            platform: req.session.platform
-        };
-        if (lineId) {
-            item.lineId = lineId;
-            item.remind = true;
-        }
+        item.description = JSON.stringify(regist_data, null, 2);
+        item.platform  = req.session.platform;
+
         const params = {
             TableName: 'TrashSchedule',
             Item: item
         };
         logger.debug('regist parameter:',params);
-        dynamoClient.put(params,(err,data)=>{
+        dynamoClient.put(params,(err)=>{
             if(err) {
                 logger.error(`DB Insert Error\n${err}`);
-                errorRedirect(res, 400, '登録に失敗しました。');
+                errorRedirect(res, 500, '登録に失敗しました。');
                 return;
             } else {
-                logger.info(`Regist user(${user_id}\n${JSON.stringify(regist_data)})`);
+                logger.info(`Regist user(${JSON.stringify(item)})`);
                 // Googleアシスタントからの登録はfirestore登録後にリダイレクトする
                 if (req.session.platform === 'google') {
-                    logger.debug(`regist firestore: ${user_id},${regist_data}`);
+                    logger.debug(`regist firestore: ${item.id},${regist_data}`);
                     firestore.runTransaction(t => {
                         const params = {
-                            id: user_id,
+                            id: item.id,
                             data: regist_data
                         };
-                        if (lineId) {
-                            params.lineId = lineId;
-                            params.remind = true;
-                        }
-                        t.set(firestore.collection('schedule').doc(user_id), params);
+                        t.set(firestore.collection('schedule').doc(item.id), params);
                         return Promise.resolve('Regist Complelete');
                     }).then(() => {
-                        logger.info(`Regist user(${user_id}\n${JSON.stringify(regist_data)})`);
-                        const redirect_url = `${req.session.redirect_uri}#state=${req.session.state}&access_token=${user_id}&client_id=${req.session.client_id}&token_type=Bearer`;
+                        logger.info(`Regist user(${item.id}\n${JSON.stringify(regist_data)})`);
+                        const redirect_url = `${req.session.redirect_uri}#state=${req.session.state}&access_token=${item.id}&client_id=${req.session.client_id}&token_type=Bearer`;
                         logger.debug(redirect_url);
-                        res.redirect(redirect_url);
+                        res.status(200).end(redirect_url);
                     }).catch(err => {
                         logger.error(`DB Insert Error\n${err}`);
-                        errorRedirect(res, 400, '登録に失敗しました。');
+                        errorRedirect(res, 500, '登録に失敗しました。');
                         return;
                     });
                 }  else {
-                    const redirect_url = `${req.session.redirect_uri}#state=${req.session.state}&access_token=${user_id}&client_id=${req.session.client_id}&token_type=Bearer`;
+                    const redirect_url = `${req.session.redirect_uri}#state=${req.session.state}&access_token=${item.id}&client_id=${req.session.client_id}&token_type=Bearer`;
                     logger.debug(redirect_url);
-                    res.redirect(redirect_url);
+                    res.status(200).end(redirect_url);
                     return;
                 }
             }
         });
-
-
     } else {
         logger.error('submit session state is not match.');
         errorRedirect(res, 400, '不正なリクエストです');
