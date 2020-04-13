db07a6a78832db65f3326ef998b66b962d2c0e21
quotto <5211139+quotto@users.noreply.github.com>
2020-04-14 00:22:30 +0900

    DB格納値の妥当性を検証できていないため、モック内で値を保管し検証するようにテストを修正

--- backend/src/__test__/oauth_request.test.js
+++ backend/src/__test__/oauth_request.test.js
@@ -1,14 +1,15 @@
 const error_def = require("../error_def");
-jest.mock("../dbadapter",()=>(
-    {
-        saveSession: async(session)=>{
-            if(session.id === "sessionid-001") {
-                return true;
-            }
-            return false;
-        }
+
+const mockResult = [];
+jest.mock("../dbadapter");
+const db = require("../dbadapter");
+db.saveSession.mockImplementation(async(session)=>{
+    mockResult[session.id] = session;
+    if (session.id === "sessionid-001" || session.id === "sessionid-002") {
+        return true;
     }
-));
+    return false;
+});
 describe("oauth_request", () => {
     const oauth_request = require("../oauth_request");
     it("セッションID新規発行", async () => {
@@ -24,6 +25,15 @@ describe("oauth_request", () => {
         const headers = response.headers;
         expect(headers.Location).toBe("https://accountlink.mythrowaway.net/v5/index.html");
         expect(headers["Set-Cookie"]).toBe("throwaway-session=sessionid-001;max-age=3600;");
+
+        // 保存したセッション
+        const session = mockResult["sessionid-001"];
+        expect(session.state).toBe("123456");
+        expect(session.client_id).toBe("alexa-skill");
+        expect(session.redirect_uri).toBe("https://xxxx.com");
+        expect(session.platform).toBe("amazon");
+        expect(session.version).toBe("5");
+        expect(session.expire).toBe(99999999);
     });
     it("有効なセッションIDを既に利用している場合", async () => {
         const response = await oauth_request({
@@ -32,11 +42,20 @@ describe("oauth_request", () => {
             redirect_uri: "https://xxxx.com",
             platform: "amazon",
             version: "5"
-        }, { id: "sessionid-001", expire: 99999999 }, false);
+        }, { id: "sessionid-002", expire: 99999999 }, false);
         expect(response.statusCode).toBe(301);
         const headers = response.headers;
         expect(headers.Location).toBe("https://accountlink.mythrowaway.net/v5/index.html");
         expect(headers["Set-Cookie"]).toBe(undefined);
+
+        // 保存したセッション
+        const session = mockResult["sessionid-002"];
+        expect(session.state).toBe("123456");
+        expect(session.client_id).toBe("alexa-skill");
+        expect(session.redirect_uri).toBe("https://xxxx.com");
+        expect(session.platform).toBe("amazon");
+        expect(session.version).toBe("5");
+        expect(session.expire).toBe(99999999);
     });
     it("セッションの保存に失敗した", async () => {
         const response = await oauth_request({
@@ -45,7 +64,7 @@ describe("oauth_request", () => {
             redirect_uri: "https://xxxx.com",
             platform: "amazon",
             version: "5"
-        }, { id: "sessionid-002", expire: 99999999 }, false);
+        }, { id: "sessionid-003", expire: 99999999 }, false);
         expect(response.statusCode).toBe(301);
         const headers = response.headers;
         expect(headers.Location).toBe(error_def.ServerError.headers.Location);
--- backend/src/__test__/register.test.js
+++ backend/src/__test__/register.test.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-unused-vars */
 const property = require("../property");
-jest.mock("../dbadapter",()=>(
-    {
-        publishId: async()=>{return "id-001"},
-        putTrashSchedule: async()=>{return true},
-        deleteSession: async()=>{return true},
-        putAuthorizationCode: async(user_id,client_id,redirect_uri)=>{
+
+// データ登録系処理の結果確認のための入れ物
+const mockResult = {};
+jest.mock("../dbadapter");
+const db = require("../dbadapter");
+db.publishId.mockImplementation(async()=>{return "id001"});
+db.putTrashSchedule.mockImplementation(async(item)=>{
+            mockResult[item.id] = item;
+            return true;
+        });
+db.deleteSession.mockImplementation(async(_sessionid)=>{return true});
+db.putAuthorizationCode.mockImplementation(async(user_id,client_id,redirect_uri)=>{
             return {
                 code: "12345",
                 user_id: "id001",
@@ -14,8 +20,7 @@ jest.mock("../dbadapter",()=>(
                 expiresin: 123456789
             }
         }
-    }
-));
+);
 const register = require("../register");
 describe('register', () => {
     it('正常なリクエスト', async () => {
@@ -27,6 +32,52 @@ describe('register', () => {
         expect(response.body).toBe('https://xxxx.com?state=state-value&code=12345');
         expect(response.headers['Access-Control-Allow-Origin']).toBe(property.URL_ACCOUNT_LINK);
         expect(response.headers['Access-Control-Allow-Credentials']).toBe(true);
+
+        // TrashScheduleに登録されたデータの確認
+        expect(mockResult["id001"].id).toBe("id001");
+        expect(mockResult["id001"].platform).toBe("amazon");
+        expect(mockResult["id001"].description).toBe(JSON.stringify([{ type: 'burn', schedules: [{ type: 'weekday', value: '0' }] }], null, 2) );
+    });
+    it('サインイン済み,id無し', async () => {
+        const response = await register({ data: [{ type: 'burn', schedules: [{ type: 'weekday', value: '0' }] }] },
+            { id: 'sessionid-001', redirect_uri: 'https://xxxx.com', state: 'state-value', client_id: 'alexa-skill', platform: 'amazon' ,
+            userInfo: {
+                signinId: "signinId002",
+                signinService: "google"
+            }
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBe('https://xxxx.com?state=state-value&code=12345');
+        expect(response.headers['Access-Control-Allow-Origin']).toBe(property.URL_ACCOUNT_LINK);
+        expect(response.headers['Access-Control-Allow-Credentials']).toBe(true);
+
+        // TrashScheduleに登録されたデータの確認
+        expect(mockResult["id001"].id).toBe("id001");
+        expect(mockResult["id001"].platform).toBe("amazon");
+        expect(mockResult["id001"].description).toBe(JSON.stringify([{ type: 'burn', schedules: [{ type: 'weekday', value: '0' }] }],null,2));
+        expect(mockResult["id001"].signinId).toBe("signinId002");
+        expect(mockResult["id001"].signinService).toBe("google");
+    });
+    it('サインイン済み,idあり', async () => {
+        const response = await register({ data: [{ type: 'burn', schedules: [{ type: 'weekday', value: '0' }] }] },
+            { id: 'sessionid-001', redirect_uri: 'https://xxxx.com', state: 'state-value', client_id: 'alexa-skill', platform: 'amazon' ,
+            userInfo: {
+                id: "id003",
+                signinId: "signinId002",
+                signinService: "google"
+            }
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBe('https://xxxx.com?state=state-value&code=12345');
+        expect(response.headers['Access-Control-Allow-Origin']).toBe(property.URL_ACCOUNT_LINK);
+        expect(response.headers['Access-Control-Allow-Credentials']).toBe(true);
+
+        // TrashScheduleに登録されたデータの確認
+        expect(mockResult["id003"].id).toBe("id003");
+        expect(mockResult["id003"].platform).toBe("amazon");
+        expect(mockResult["id003"].description).toBe(JSON.stringify([{ type: 'burn', schedules: [{ type: 'weekday', value: '0' }] }],null,2));
+        expect(mockResult["id003"].signinId).toBe("signinId002");
+        expect(mockResult["id003"].signinService).toBe("google");
     });
     it("bodyがnull",async()=>{
         const register = require("../register");
