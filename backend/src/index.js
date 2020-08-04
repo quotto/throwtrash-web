c7d5c9fceb15291ae3e42f6792c791a2d9d61fda
quotto <5211139+quotto@users.noreply.github.com>
2020-08-04 23:33:59 +0900

    アクセストークンのTTLを30日、リフレッシュトークンのTTLを180日に延長

--- backend/src/index.js
+++ backend/src/index.js
@@ -102,8 +102,9 @@ exports.handler = async function(event,context) {
             const result =  await enable_skill(event.queryStringParameters,session, event.requestContext.stage);
             return result;
         } catch(err) {
-            logger.error(`Unexpected error(enable_skill)->${err}`)
+            logger.error(`Unexpected error(enable_skill)->${err.stack || err}`)
             logger.error(event)
+            logger.error(session)
             logger.error(context)
         }
     }
