57665edc6a2a949c9dcc3374c3b723788d6f0bed
quotto <5211139+quotto@users.noreply.github.com>
2022-12-25 06:04:15 +0000

    スケジュール無しでユーザー登録可能にする

--- api/src/__tests__/register.test.ts
+++ api/src/__tests__/register.test.ts
@@ -45,24 +45,28 @@ import register from "../register";
 
 describe("register",()=>{
     it("正常終了",async()=>{
-        const mockedInsertTrashSchedule = jest.mocked(dbadapter.insertTrashSchedule).mockImplementation(async (_: TrashScheduleItem) => { return true });
-        const result = await register({ description: JSON.stringify(data001), platform: "android" }) as APIGatewayProxyStructuredResultV2;
+        const mockedInsertTrashSchedule = jest.mocked(dbadapter.insertTrashSchedule).mockImplementation(async (_: TrashScheduleItem, timestamp) => { return true });
+        const result = await register({ platform: "android" }) as APIGatewayProxyStructuredResultV2;
         const body = JSON.parse(result.body!);
         expect(result.statusCode).toBe(200);
         expect(body.id.length).toBeGreaterThan(0);
 
         expect(mockedInsertTrashSchedule).toBeCalledWith(expect.objectContaining({
             id: expect.any(String),
-            description: JSON.stringify(data001),
+            description: JSON.stringify([]),
             platform: "android",
         }), expect.any(Number));
     });
-    it("登録するスケジュールにエラーがある場合はユーザーエラー",async()=>{
-        const result = await register({description: JSON.stringify([]), platform: "android"}) as APIGatewayProxyStructuredResultV2;
+    it("platformの指定が無い場合はユーザーエラー",async()=>{
+        const result = await register({}) as APIGatewayProxyStructuredResultV2;
+        expect(result.statusCode).toBe(400);
+    });
+    it("platformが空の場合はエラー",async()=>{
+        const result = await register({platform: ""}) as APIGatewayProxyStructuredResultV2;
         expect(result.statusCode).toBe(400);
     });
     it("DB登録異常の場合はサーバエラー",async()=>{
-        jest.mocked(dbadapter.insertTrashSchedule).mockImplementation(async (_: TrashScheduleItem) => { return false });
+        jest.mocked(dbadapter.insertTrashSchedule).mockImplementation(async (_: TrashScheduleItem, timestamp) => { return false });
         const result = await register({ description: JSON.stringify(data001), platform: "error" }) as APIGatewayProxyStructuredResultV2;
         expect(result.statusCode).toBe(500);
     });
--- api/src/register.ts
+++ api/src/register.ts
@@ -1,42 +1,28 @@
-import { APIGatewayProxyResultV2 } from "aws-lambda";
+import { APIGatewayProxyResultV2, APIGatewayProxyEventQueryStringParameters } from "aws-lambda";
 import * as  common from "trash-common";
 import dbadapter from "./dbadapter";
 import { RegisteredTrashScheduleItem } from "./interface";
 const logger = common.getLogger();
 
-export default async(trashScheduleItem: RegisteredTrashScheduleItem): Promise<APIGatewayProxyResultV2> => {
-    logger.info(`Register Data -> ${JSON.stringify(trashScheduleItem)}`);
+export default async (params: APIGatewayProxyEventQueryStringParameters): Promise<APIGatewayProxyResultV2> => {
+    if(typeof(params.platform) === "undefined" || params.platform === null || params.platform.length === 0) {
+        logger.error(`invalid parameter: platform -> ${params.platform}`);
+        return { statusCode: 400 };
+    }
+    const id = common.generateUUID("-");
+    logger.debug(`publish user id -> ${id}`)
 
-    try {
-        const id = common.generateUUID("-");
-        logger.debug(`publish user id -> ${id}`)
+    const timestamp = new Date().getTime();
+    const registeredItem = {
+        id: id,
+            description: JSON.stringify([]),
+        platform: params.platform
+    }
 
-        // dataフォーマットチェックに成功したら登録する
-        if(common.checkTrashes(JSON.parse(trashScheduleItem.description))) {
-            const timestamp =  new Date().getTime();
-            logger.debug(`put new item -> ${JSON.stringify(trashScheduleItem)}`)
-
-            const registeredItem = {
-                id: id,
-                description: trashScheduleItem.description,
-                platform: trashScheduleItem.platform
-            }
-
-            logger.info(`register user: ${JSON.stringify(registeredItem)}, timestamp: ${timestamp}`);
-            if(await dbadapter.insertTrashSchedule(registeredItem, timestamp)) {
-                return {statusCode:200, body: JSON.stringify({id: id, timestamp: timestamp})};
-            } else {
-                return {statusCode: 500};
-            }
-
-        } else {
-            logger.error(`validation failed: ${trashScheduleItem.description}`);
-            return {
-                statusCode: 400
-            };
-        }
-    } catch(err: any) {
-        logger.error(err);
-        return {statusCode:400};
+    logger.info(`register user: ${JSON.stringify(registeredItem)}, timestamp: ${timestamp}`);
+    if (await dbadapter.insertTrashSchedule(registeredItem, timestamp)) {
+        return { statusCode: 200, body: JSON.stringify({ id: id, timestamp: timestamp }) };
+    } else {
+        return { statusCode: 500 };
     }
 }
