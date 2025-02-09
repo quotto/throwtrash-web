f2f4b6a7e7bc6a244103407abbff93b7c5e8cdc3
quotto <5211139+quotto@users.noreply.github.com>
2025-02-09 23:50:39 +0900

    Add mobile_signin_id parameter to register function and update tests

--- api/src/__tests__/register.test.ts
+++ api/src/__tests__/register.test.ts
@@ -5,37 +5,10 @@ import * as common from "trash-common";
 const logger = common.getLogger();
 logger.setLevel_DEBUG();
 
-const data001 = [
-    {
-        id: "1234567",
-        type: "burn",
-        trash_val: "",
-        schedules: [
-            {
-                type: "weekday",
-                value: "0"
-            },{
-                type: "biweek",
-                value: "1-1"
-            }
-        ]
-    },{
-        id: "8901234",
-        type: "other",
-        trash_val: "生ゴミ",
-        schedules: [
-            {
-                type: "evweek",
-                value: {
-                    weekday: "2",
-                    start: "2020-03-10"
-                }
-            }
-        ]
-    }
-];
 
 const mockId = "1234567891";
+const mockFirebaseAccountId = "firebase-account-id";
+
 jest.spyOn(common, "generateUUID").mockImplementation((separator)=>mockId);
 
 jest.mock("../dbadapter");
@@ -46,7 +19,7 @@ import register from "../register";
 describe("register",()=>{
     it("正常終了",async()=>{
         const mockedInsertTrashSchedule = jest.mocked(dbadapter.insertTrashSchedule).mockImplementation(async (_: TrashScheduleItem, timestamp) => { return true });
-        const result = await register("{\"platform\": \"android\"}") as APIGatewayProxyStructuredResultV2;
+        const result = await register("{\"platform\": \"android\"}", mockFirebaseAccountId) as APIGatewayProxyStructuredResultV2;
         const body = JSON.parse(result.body!);
         expect(result.statusCode).toBe(200);
         expect(body.id.length).toBeGreaterThan(0);
@@ -55,24 +28,25 @@ describe("register",()=>{
             id: expect.any(String),
             description: JSON.stringify([]),
             platform: "android",
+            mobile_signin_id: mockFirebaseAccountId,
         }), expect.any(Number));
     });
     it("platformの指定が無い場合はユーザーエラー",async()=>{
-        const result = await register("{}") as APIGatewayProxyStructuredResultV2;
+        const result = await register("{}", mockFirebaseAccountId) as APIGatewayProxyStructuredResultV2;
         expect(result.statusCode).toBe(400);
     });
     it("platformが空の場合はユーザーエラー",async()=>{
-        const result = await register("{\"platform\": \"\"}") as APIGatewayProxyStructuredResultV2;
+        const result = await register("{\"platform\": \"\"}", mockFirebaseAccountId) as APIGatewayProxyStructuredResultV2;
         expect(result.statusCode).toBe(400);
     });
     it("JSONのSyntaxエラーの場合はユーザーエラー", async () => {
-        const result = await register("platform") as APIGatewayProxyStructuredResultV2;
+        const result = await register("platform", mockFirebaseAccountId) as APIGatewayProxyStructuredResultV2;
         expect(result.statusCode).toBe(400);
     });
 
     it("DB登録異常の場合はサーバエラー",async()=>{
         jest.mocked(dbadapter.insertTrashSchedule).mockImplementation(async (_: TrashScheduleItem, timestamp) => { return false });
-        const result = await register("{\"platform\": \"error\"}") as APIGatewayProxyStructuredResultV2;
+        const result = await register("{\"platform\": \"error\"}", mockFirebaseAccountId) as APIGatewayProxyStructuredResultV2;
         expect(result.statusCode).toBe(500);
     });
 });
--- api/src/register.ts
+++ api/src/register.ts
@@ -1,30 +1,31 @@
 import { APIGatewayProxyResultV2, APIGatewayProxyEventQueryStringParameters, APIGatewayEventDefaultAuthorizerContext, APIGatewayProxyEventBase } from "aws-lambda";
-import * as  common from "trash-common";
+import * as common from "trash-common";
 import dbadapter from "./dbadapter";
 import { RegisteredTrashScheduleItem } from "./interface";
 const logger = common.getLogger();
 
-export default async (event_body_str: string) => {
+export default async (event_body_str: string, firebaseAccountId: string) => {
     let event_body;
     try {
-        event_body =JSON.parse(event_body_str)
-        if(typeof(event_body.platform) === "undefined" || event_body.platform.length === 0) {
+        event_body = JSON.parse(event_body_str);
+        if (typeof(event_body.platform) === "undefined" || event_body.platform.length === 0) {
             logger.error(`invalid parameter: platform -> ${event_body.platform}`);
             return { statusCode: 400 };
         }
     } catch (e: any) {
-        logger.error(e)
+        logger.error(e);
         return { statusCode: 400 };
     }
-    const id = common.generateUUID("-");
-    logger.debug(`publish user id -> ${id}`)
 
+    const id = common.generateUUID("-");
+    logger.debug(`publish user id -> ${id}`);
     const timestamp = new Date().getTime();
     const registeredItem = {
         id: id,
         description: JSON.stringify([]),
-        platform: event_body.platform
-    }
+        platform: event_body.platform,
+        mobile_signin_id: firebaseAccountId,
+    };
 
     logger.info(`register user: ${JSON.stringify(registeredItem)}, timestamp: ${timestamp}`);
     if (await dbadapter.insertTrashSchedule(registeredItem, timestamp)) {
@@ -32,4 +33,4 @@ export default async (event_body_str: string) => {
     } else {
         return { statusCode: 500 };
     }
-}
+};
